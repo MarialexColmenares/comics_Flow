822d0e67a8e44a67bf18b40696486c0e64ac47a6
Mariallex <marialexcolmenares5@gmail.com>
2026-04-10 21:01:19 -0400

    personalice un poco mas la parte de los contenedores de comcs

--- src/components/cardComic.jsx
+++ src/components/cardComic.jsx
@@ -2,37 +2,33 @@ const cardStyle = {
   display: "flex",
   flexDirection: "column",
   alignItems: "center",
-  width: "200px",
-  margin: "10px", 
+  width: "180px",
+  margin: "10px",
   border: "1px solid #ddd",
   borderRadius: "8px",
-  overflow: "hidden"
+  overflow: "hidden",
 };
 
 const imageStyle = {
-  width: "100%",            // La imagen llena el ancho de la tarjeta
+  width: "100%",
   height: "auto",
   display: "block",
 };
 
 const dataStyle = {
-  width: "100%",            // El fondo rojo cubrirá todo el ancho
+  width: "100%",
   color: "white",
   backgroundColor: "red",
-  textAlign: "center",      // Centra el texto del título
-  padding: "10px 0",        // Espaciado interno
+  textAlign: "center",
+  padding: "10px 0",
 };
 
-function CardComic() {
+function CardComic({ imagen, title }) {
   return (
     <div style={cardStyle}>
-      <img
-        style={imageStyle}
-        src="https://www.superherodb.com/pictures2/portraits/10/100/1.jpg"
-        alt="A-Bomb Character"
-      />
+      <img style={imageStyle} src={imagen} alt={title} />
       <div style={dataStyle}>
-        <h2 style={{ margin: 0, fontSize: "20px" }}>A-Bomb</h2>
+        <h2 style={{ margin: 0, fontSize: "20px" }}>{title}</h2>
       </div>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import Fondo from "../assets/fondo.jpeg";
 import CardComic from "../components/cardComic";
+import AbrirComic from "../components/AbrirComic";
+import portada from "../assets/supergril2026_Character6/portada.jpg";
 
 const containerComics = {
   display: "flex", // Alinea los hijos en horizontal
@@ -17,11 +19,58 @@ function Home() {
         </div>
         <div>
           <h1>Comics flow</h1>
+          <h3>terror</h3>
           <div style={containerComics}>
-            <CardComic />
-            <CardComic />
-            <CardComic />
-            <CardComic />
+            <CardComic
+              title={"nightwing"}
+              imagen={
+                "https://marmota.me/wp-content/uploads/WP-manga/data/manga_6297af0a4882b/163c5026eb9cb38bd07b203774902312/Nightwing-135-(2026)-(Digital)-(Pyrate-DCP)-0001.jpg"
+              }
+            />
+            <CardComic
+              title={"Absolute Wonder Woman"}
+              imagen={
+                "https://marmota.me/wp-content/uploads/WP-manga/data/manga_672818fb203f4/e65a26d08ecee94cdb1d8be50a282b8f/Absolute-Wonder-Woman-2026-Annual-001-(2026)-(Digital)-(Pyrate-DCP)-0001.jpg"
+              }
+            />
+            <CardComic
+              title={"Ultimate Endgame #1"}
+              imagen={
+                "https://marmota.me/wp-content/uploads/WP-manga/data/manga_69b8b825d1454/aa5e8193591717452ad849e6ab3323b6/Ultimate-Endgame-001-(2026)-(Digital)-(Shan-Empire)-00001.jpg"
+              }
+            />
+            <CardComic
+              title={" Superman "}
+              imagen={
+                "https://marmota.me/wp-content/uploads/WP-manga/data/manga_64304a99b9387/01519453d8a612c05de468f7acd69923/Superman-035-(2026)-(Digital)-(Pyrate-DCP)-0001.jpg"
+              }
+            />
+
+            <AbrirComic />
+          </div>
+          <h3>chicas</h3>
+          <div style={containerComics}>
+            <CardComic title={"Supergril 2026"} imagen={portada} />
+            <CardComic
+              title={"batman"}
+              imagen={
+                "https://marmota.me/wp-content/uploads/WP-manga/data/manga_682bee1d1dafb/1b6cbe285322e9f83f5ceeebbce954e7/Supergirl-007-(2026)---0001.jpg"
+              }
+            />
+            <CardComic
+              title={"Hellboy in love"}
+              imagen={
+                "https://marmota.me/wp-content/uploads/WP-manga/data/manga_69d8670fda56b/412a8a7a1bf4c2c2b073b610cf32deda/Hellboy-in-Love-001.jpg"
+              }
+            />
+            <CardComic
+              title={"Absolute Superman"}
+              imagen={
+                "https://marmota.me/wp-content/uploads/WP-manga/data/manga_67367bba6e6c2/fa9579bc9b3e63daed5f1200eb9aaadc/Absolute-Superman-010-(2025)-(Digital)-(Shan-Empire)-00001.jpg"
+              }
+            />
+
+            <AbrirComic />
           </div>
         </div>
       </section>
